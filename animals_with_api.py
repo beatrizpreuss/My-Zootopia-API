5215baba293d4853c30ec0076300155c782dfbff
Beatriz Preuss <beatrizpreuss@Beatrizs-MacBook-Air.local>
2025-05-27 16:50:30 +0200

    Milestone 2 - add user choice of animal

--- animals_with_api.py
+++ animals_with_api.py
@@ -2,10 +2,15 @@ import requests
 
 API_KEY = 'tX+p7IesWxHN16/NqL2HRg==GrkHAE4JXFrM96K8'
 ANIMAL_URL = 'https://api.api-ninjas.com/v1/animals?name='
-ANIMAL = 'Fox'
 
-def get_animals_from_api():
-    res = requests.get(f'{ANIMAL_URL}{ANIMAL}', headers={'X-Api-Key': API_KEY})
+
+def get_user_input():
+    user_animal = input("Enter a name of an animal: ")
+    return user_animal
+
+
+def get_animals_from_api(user_animal):
+    res = requests.get(f'{ANIMAL_URL}{user_animal}', headers={'X-Api-Key': API_KEY})
     animals_resp = res.json()
     return animals_resp
 
@@ -16,11 +21,11 @@ def get_html(html_file):
     return html.read()
 
 
-def get_info():
+def get_info(user_animal):
     """ Gets information about each animal in the json file """
     # animals_data = load_data('animals_resp.json')
     output = ""
-    info_from_api = get_animals_from_api()
+    info_from_api = get_animals_from_api(user_animal)
     for animal in info_from_api:
         try:
             output += (f'<li class="cards__item">'
@@ -38,24 +43,26 @@ def get_info():
     return output
 
 
-def replace_info():
+def replace_info(user_animal):
     """ Replaces the placeholder in the original html with the
     information about the animals """
     original_html = get_html("animals_template.html")
-    animals_info = get_info()
+    animals_info = get_info(user_animal)
     new_html = original_html.replace("__REPLACE_ANIMALS_INFO__", animals_info)
     return new_html
 
 
-def write_html():
+def write_html(user_animal):
     """ Writes a new html file with the replaced information """
-    info = replace_info()
+    info = replace_info(user_animal)
     with open("animals.html", "w") as new_html:
         new_html.write(info)
 
 
 def main():
-    write_html()
+    user_animal = get_user_input()
+    write_html(user_animal)
+    print("Website was successfully generated to the file animals.html")
 
 
 if __name__ == "__main__":
